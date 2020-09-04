7e739ed958f58c718a032355889b89f9b806d8cf
starlesssss <61955474+starlesssss@users.noreply.github.com>
2020-09-04 14:08:32 +0900

    ???

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { SubjectList, SubjectInput, TimeTable, SubjectNow } from './component/index';
+import { default as storage } from './stores/schedule'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
     <>
       <div className="App noselect" class="container border border-primary pt-2 pb-3 pl-3 pr-3">
